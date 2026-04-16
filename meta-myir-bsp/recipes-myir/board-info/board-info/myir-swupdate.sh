7ae0dfb139075ca9de3a8e8d5f7acd464cfc7fe8
duxy <568988005@qq.com>
2026-04-16 15:04:34 +0800

    FIX: use myir publick key and support for http OTA

--- meta-myir-bsp/recipes-myir/board-info/board-info/myir-swupdate.sh
+++ meta-myir-bsp/recipes-myir/board-info/board-info/myir-swupdate.sh
@@ -14,6 +14,7 @@ check_root_part_cmdline()
 	do
 		if [[ `echo $i | grep "root="` != "" ]];then
 			current_rootfs=${i##*/}
+			echo "current_rootfs: $current_rootfs"
 		fi
 	done
 
@@ -46,27 +47,31 @@ check_need_replace_env() {
 
 function_on_different_part()
 {
-        case ${current_rootfs:0-1} in 
+	part_num=${current_rootfs##*p}
+
+	mkdir -p /etc/swupdate/conf.d
+
+	case "$part_num" in
         ${ROOTFS_A_PART})
-                echo "rootfs A part"
-		fw_setenv swu_mode 0
-		fw_setenv boot_limit 3
-		fw_setenv mmcbootpart ${ROOTFS_A_PART}
-		fw_setenv mmcbootpart_back ${ROOTFS_B_PART}
-		echo "SWUPDATE_ARGS=\"-v -d -uhttp://192.168.1.20/MYD-LMX9X-IMAGE/myd-lmx9x.swu -e stable,main\"" > /etc/myir-swupdate.cfg
+		echo "rootfs A part"
+		cat > /etc/swupdate/conf.d/99-myir-swupdate.conf << 'EOF'
+SWUPDATE_ARGS="-v -d -uhttp://192.168.40.241/myir_1.0_LF_v6.12.49_2.1.0_singlecopy_emmc_image_20260415_sign.swu -e stable,dualA_to_dualB"
+SWUPDATE_WEBSERVER_ARGS=""
+EOF
         ;;
         ${ROOTFS_B_PART})
-                echo "rootfs B part" 
-		fw_setenv swu_mode 0
-		fw_setenv boot_limit 3
-		fw_setenv mmcbootpart ${ROOTFS_B_PART}
-		fw_setenv mmcbootpart_back ${ROOTFS_A_PART}
-		echo "SWUPDATE_ARGS=\"-v -d -uhttp://192.168.1.20/MYD-LMX9X-IMAGE/myd-lmx9x.swu -e stable,alt\"" > /etc/myir-swupdate.cfg
+                echo "rootfs B part"
+		cat > /etc/swupdate/conf.d/99-myir-swupdate.conf << 'EOF'
+SWUPDATE_ARGS="-v -d -uhttp://192.168.40.241/myir_1.0_LF_v6.12.49_2.1.0_singlecopy_emmc_image_20260415_sign.swu -e stable,dualB_to_dualA"
+SWUPDATE_WEBSERVER_ARGS=""
+EOF
         ;;
         *)
                 echo "con not distinguish part"
                 exit 1
         esac
+
+	cat /etc/swupdate/conf.d/99-myir-swupdate.conf
 }
 
 
@@ -76,6 +81,3 @@ check_need_replace_env
 
 function_on_different_part
 
-
-source /etc/myir-swupdate.cfg
-swupdate $SWUPDATE_ARGS
--- meta-myir-bsp/recipes-myir/swupdate/swupdate_%.bbappend
+++ meta-myir-bsp/recipes-myir/swupdate/swupdate_%.bbappend
@@ -1,3 +1,6 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${BPN}:"
 SRC_URI += "file://defconfig"
+SRC_URI += "file://swu_public.pem"
+SRC_URI += "file://swupdate.cfg"
+
 DEPENDS:append = " gpgme"
